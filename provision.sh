ae3ac7ef760d2d23533775e3adf0aa05c35f3a8d
[bea] <[1@gmail.com]>
2020-10-28 13:00:54 +0100

    copiando index.html a guest, borrando directorio original y creando enlace blando, actualizando provision.sh y comprobando que funciona correctamente

--- provision.sh
+++ provision.sh
@@ -1,4 +1,10 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
+sudo cp -R /usr/share/nginx/www /vagrant
+sudo rm -rf /usr/share/nginx/www
+
+cd /usr/share/nginx
+sudo ln -s /vagrant/www
+
 sudo service nginx start
