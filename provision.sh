b21f6585dde6c764ef8e0c88f774af813b3fc729
[bea] <[1@gmail.com]>
2020-10-28 13:31:09 +0100

    editando default, borrando enlace simbolico, destruyendo box y lo volvemos a levantar

--- provision.sh
+++ provision.sh
@@ -1,10 +1,13 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
-sudo cp -R /usr/share/nginx/www /vagrant
-sudo rm -rf /usr/share/nginx/www
+# sudo rm -rf /etc/ngnix/sites-enabled
+# sudo cp -R /usr/share/nginx/www /vagrant
+# sudo rm -rf /usr/share/nginx/www
 
-cd /usr/share/nginx
-sudo ln -s /vagrant/www
+# sudo cp -R /vagrant/sites-enabled /etc/nginx/
+
+# cd /usr/share/nginx
+# sudo ln -s /vagrant/www
 
 sudo service nginx start
